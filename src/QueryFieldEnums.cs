06e4b6c8392adfeb14f33dc642e61b223499c5e9
Frank Deasey <snake-plissken@users.noreply.github.com>
2017-12-02 12:58:58 -0500

    Added query field enums

---
+++ src/QueryFieldEnums.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace cSharpGeocodio
+{
+	public enum QueryCongressional
+	{
+		Yes
+		, No
+	}
+
+	public enum QueryStateLegislature
+	{
+		Yes
+		, No
+	}
+
+	public enum QuerySchoolDistrict
+	{
+		Yes
+		, No
+	}
+
+	public enum QueryCensusInfo
+	{
+		Yes
+		, No
+	}
+
+	public enum QueryTimeZone
+	{
+		Yes
+		, No
+	}
+}
