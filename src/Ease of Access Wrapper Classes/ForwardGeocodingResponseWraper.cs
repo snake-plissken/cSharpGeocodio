f588dcce109f8c1cfc11cd8c6a8b721d79faf0e9
Frank Deasey <snake-plissken@users.noreply.github.com>
2018-06-05 21:18:04 -0400

    Added ease of access classes to wrap cumbersome response

---
+++ src/Ease of Access Wrapper Classes/ForwardGeocodingResponseWraper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using cSharpGeocodio.ForwardGeocodingObjects;
+
+namespace cSharpGeocodio
+{
+	public static class ForwardGeocodingResponseWraper
+	{
+		public static Dictionary<string, GeoCodeInfo[]> MakeForwardResultsDict(BatchForwardGeoCodeResult results)
+		{
+			Dictionary<string, GeoCodeInfo[]> resultsDict = new Dictionary<string, GeoCodeInfo[]>();
+
+			foreach (BatchForwardGeoCodeRecord record in results.Results)
+			{
+				//Make sure have something to use...
+				if (record.Response.Results.Length > 0)
+				{
+					if (resultsDict.ContainsKey(record.Query))
+					{
+						//How to handle this?  Make end user worry about it?
+					}
+					else
+					{
+						resultsDict.Add(record.Query, record.Response.Results);
+					}
+				}
+				//How to handle queries with no results?
+				else
+				{
+					resultsDict.Add(record.Query, new GeoCodeInfo[0]);
+				}
+			}
+			return resultsDict;
+		}
+	}
+}
---
+++ src/Ease of Access Wrapper Classes/ReverseGeocodingResponseWrapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cSharpGeocodio
+{
+	public static class ReverseGeocodingResponseWrapper
+	{
+		public static Dictionary<string, GeoCodeInfo[]> MakeReverseResultsDict(BatchReverseGeoCodingResult results)
+		{
+			Dictionary<string, GeoCodeInfo[]> resultsDict = new Dictionary<string, GeoCodeInfo[]>();
+
+			foreach (BatchReverseGeoCodeResponse response in results.Results)
+			{
+				if (response.Response.Results.Length > 0)
+				{
+					if (resultsDict.ContainsKey(response.Query))
+					{
+						//How to handle dupe key?  Make end user worry about it?
+					}
+					else
+					{
+						resultsDict.Add(response.Query, response.Response.Results);
+					}
+				}
+				else
+				{
+					resultsDict.Add(response.Query, new GeoCodeInfo[0]);
+				}
+			}
+
+			return resultsDict;
+		}
+	}
+}
