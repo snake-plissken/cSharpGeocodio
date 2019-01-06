ca040d70cd350958077a3cdf3e849033df01420d
Frank Deasey <snake-plissken@users.noreply.github.com>
2019-01-05 22:08:10 -0500

    added assemblyInfo, updated/added json backers

--- src/Categorical Json Response Backer Classes/CombinedStatisticalArea.cs
+++ src/Categorical Json Response Backer Classes/CombinedStatisticalArea.cs
@@ -3,21 +3,18 @@ using Newtonsoft.Json;
 
 namespace cSharpGeocodio
 {
-	public class MetroMicroStatisticalArea
+	public class CombinedStatisticalArea
 	{
 		[JsonConstructor]
-		public MetroMicroStatisticalArea(string name, string area_code, string type)
+		public CombinedStatisticalArea(string name, string area_code)
 		{
 			Name = name;
 			AreaCode = area_code;
-			Type = type;
 		}
 
 		[JsonProperty("name")]
 		public string Name { get; set; }
 		[JsonProperty("area_code")]
 		public string AreaCode { get; set; }
-		[JsonProperty("type")]
-		public string Type { get; set; }
 	}
 }
