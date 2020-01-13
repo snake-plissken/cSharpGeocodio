9877ba9f59f549d5dc96fffe86b87c9ffaffa8b8
Frank Deasey <snake-plissken@users.noreply.github.com>
2020-01-12 19:03:17 -0500

    upped version and assembly info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("cSharpGeocodio")]
-[assembly: AssemblyDescription("A .NET library for interfacing with Geocodio")]
+[assembly: AssemblyDescription("A .NET library for interfacing with Geocodio (https://www.geocod.io/)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("MIT License")]
+[assembly: AssemblyCopyright("MIT-BSD License")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
